3bee4709f9c5c3072bdcb120ac4faea37d00a297
Mark Jensen <mark.jensen@fhnw.edu>
2025-03-07 14:00:48 -0700

    final css edit

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const form = document.getElementById('form')
 form.addEventListener('submit', function(event) {
 event.preventDefault();
+
+let name = document.getElementById("username").value
+let message = document.getElementById("message");
+
 });
